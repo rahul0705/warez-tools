4d2faf2860eb6a11b1c52b333870a23d6f3754e7
Rahul Mohandas <rahul@rahulmohandas.com>
2015-05-09 22:17:44 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "warez-tools",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Rahul Mohandas",
+    author_email = "rahul@rahulmohandas.com",
+    description = "Tools to handle Warez Standards",
+    license = "MIT"
+)
