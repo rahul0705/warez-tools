77bd4defa621fdfe7a479516e204d2df8f37090a
Rahul Mohandas <rahul@rahulmohandas.com>
2015-05-10 17:33:32 -0400

    shortend unittests

--- tests/test_file.py
+++ tests/test_file.py
@@ -29,177 +29,153 @@ class TestWarezFile(unittest.TestCase):
             if exc.errno != errno.ENOENT:
                 raise
 
-    def test_remove_group_no_change_lowercase(self):
+    def test_remove_group_no_change(self):
         """Test to check that a groupless file does not get changed lowercase
         """
         test_filename_without_group = "test.file.name.with.no.group.mp4"
+
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_without_group))
+                                           test_filename_without_group.lower()))
         warezfile.remove_group()
         self.assertEqual(os.path.basename(warezfile.filename),
                          test_filename_without_group,
                          msg="Filename changed")
-
-    def test_remove_group_no_change_uppercase(self):
-        """Test to check that a groupless file does not get changed uppercase
-        """
-        test_filename_without_group = "TEST.FILE.NAME.WITH.NO.GROUP.MP4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_without_group))
+                                           test_filename_without_group.upper()))
         warezfile.remove_group()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_without_group,
+                         test_filename_without_group.upper(),
                          msg="Filename changed")
 
-    def test_remove_group_with_group_lowercase(self):
+    def test_remove_group_with_group(self):
         """Test removal of scene group lowercase
         """
         test_filename_with_group = "test.file.name.with.group-groupname.mp4"
         test_filename_without_group = "test.file.name.with.group.mp4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_with_group))
+                                           test_filename_with_group.lower()))
         warezfile.remove_group()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_without_group,
+                         test_filename_without_group.lower(),
                          msg="Group was not removed")
-
-    def test_remove_group_with_group_uppercase(self):
-        """Test removal of scene group uppercase
-        """
-        test_filename_with_group = "TEST.FILE.NAME.WITH.GROUP-GROUPNAME.MP4"
-        test_filename_without_group = "TEST.FILE.NAME.WITH.GROUP.MP4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_with_group))
+                                           test_filename_with_group.upper()))
         warezfile.remove_group()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_without_group,
+                         test_filename_without_group.upper(),
                          msg="Group was not removed")
 
-    def test_fix_show_no_change_lowercase(self):
+    def test_fix_show_no_change_one_episode(self):
         """Test to check properly formatted lowercase files don't change
         """
-        #Test correctly formatted file (Lowercase)
-        test_filename_sxxexx = "test.tv.show.s01e01.resolution.mp4"
+        #Test correctly formatted file
+        test_filename = "test.tv.show.s01e01.resolution.mp4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxexx))
+                                           test_filename.lower()))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexx,
+                         test_filename.lower(),
+                         msg="Filename changed")
+        warezfile = WarezFile(os.path.join(self.temp_dir,
+                                           test_filename.upper()))
+        warezfile.fix_show()
+        self.assertEqual(os.path.basename(warezfile.filename),
+                         test_filename.upper(),
                          msg="Filename changed")
 
-        #Test correctly formatted file (Lowercase)
-        test_filename_sxxexxexx = "test.tv.show.s01e01e02.resolution.mp4"
-        warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxexxexx))
-        warezfile.fix_show()
-        self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexxexx,
-                         msg="Filename changed")
-
-        #Test Movie
-        test_filename_movie = "test.movie.2015.resolution.mp4"
-        warezfile = WarezFile(os.path.join(self.temp_dir, test_filename_movie))
-        warezfile.fix_show()
-        self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_movie,
-                         msg="Movie file changed")
-
-    def test_fix_show_no_change_uppercase(self):
-        """Test to check properly formatted uppercase files don't change
+    def test_fix_show_no_change_two_episodes(self):
+        """Test to check properly formatted lowercase files don't change
         """
         #Test correctly formatted file
-        test_filename_sxxexx = "TEST.TV.SHOW.S01E01.RESOLUTION.mp4"
+        test_filename = "test.tv.show.s01e01e02.resolution.mp4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxexx))
+                                           test_filename.lower()))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexx,
+                         test_filename.lower(),
+                         msg="Filename changed")
+        warezfile = WarezFile(os.path.join(self.temp_dir,
+                                           test_filename.upper()))
+        warezfile.fix_show()
+        self.assertEqual(os.path.basename(warezfile.filename),
+                         test_filename.upper(),
                          msg="Filename changed")
 
-        #Test correctly formatted file
-        test_filename_sxxexxexx = "TEST.TV.SHOW.S01E01E02.RESOLUTION.mp4"
+    def test_fix_show_not_show(self):
+        """Test to check non-shows don't get modified
+        """
+        test_filename = "test.movie.2015.resolution.mp4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxexxexx))
+                                           test_filename.lower()))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexxexx,
-                         msg="Filename changed")
-
-        #Test Movie
-        test_filename_movie = "TEST.MOVIE.2015.RESOLUTION.MP4"
-        warezfile = WarezFile(os.path.join(self.temp_dir, test_filename_movie))
+                         test_filename.lower(),
+                         msg="Movie file changed")
+        warezfile = WarezFile(os.path.join(self.temp_dir,
+                                           test_filename.upper()))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_movie,
+                         test_filename.upper(),
                          msg="Movie file changed")
 
-    def test_fix_show_extra_dots_lowercase(self):
-        """Test conversion of sSS.eEE or sSS.eEE.eEE to sSSeEE or sSSeEEeEE
+    def test_fix_show_extra_dots_one_episode(self):
+        """Test conversion of sSS.eEE to sSSeEE
         """
         #Test for 1 episode
-        test_filename_sxxdotexx = "test.tv.show.s01.e01.resolution.mp4"
-        test_filename_sxxexx = "test.tv.show.s01e01.resolution.mp4"
+        test_filename_dot = "test.tv.show.s01.e01.resolution.mp4"
+        test_filename = "test.tv.show.s01e01.resolution.mp4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxdotexx))
+                                           test_filename_dot.lower()))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexx,
+                         test_filename.lower(),
+                         msg="sSS.eEE was not converted to sSSeEE")
+        warezfile = WarezFile(os.path.join(self.temp_dir,
+                                           test_filename_dot.upper()))
+        warezfile.fix_show()
+        self.assertEqual(os.path.basename(warezfile.filename),
+                         test_filename.upper(),
                          msg="sSS.eEE was not converted to sSSeEE")
 
-        #Test for 2 episodes
-        test_filename_sxxdotexxdotexx = ("test.tv.show.s01."
+    def test_fix_show_extra_dots_two_episodes(self):
+        """Test conversion of sSS.eEE.eEE to sSSeEEeEE
+        """
+        test_filename_dot = ("test.tv.show.s01."
                                          "e01.e02.resolution.mp4")
-        test_filename_sxxexxexx = "test.tv.show.s01e01e02.resolution.mp4"
+        test_filename = "test.tv.show.s01e01e02.resolution.mp4"
         warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxdotexxdotexx))
+                                           test_filename_dot.lower()))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexxexx,
+                         test_filename.lower(),
+                         msg="sSS.eEE.eEE was not converted to sSSeEEeEE")
+        warezfile = WarezFile(os.path.join(self.temp_dir,
+                                           test_filename_dot.upper()))
+        warezfile.fix_show()
+        self.assertEqual(os.path.basename(warezfile.filename),
+                         test_filename.upper(),
                          msg="sSS.eEE.eEE was not converted to sSSeEEeEE")
 
-    def test_fix_show_extra_dots_uppercase(self):
-        """Test conversion of sSS.eEE or sSS.eEE.eEE to sSSeEE or sSSeEEeEE
+    def test_fix_show_numbers_only_one_episode(self):
+        """Test conversion of SEE to sSSeEE
         """
-        #Test for 1 episode
-        test_filename_sxxdotexx = "test.tv.show.S01.E01.resolution.mp4"
-        test_filename_sxxexx = "test.tv.show.S01E01.resolution.mp4"
-        warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxdotexx))
+        test_filename_number = "test.tv.show.101.resolution.mp4"
+        test_filename = "test.tv.show.s01e01.resolution.mp4"
+        warezfile = WarezFile(os.path.join(self.temp_dir, test_filename_number))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexx,
-                         msg="SSS.EEE was not converted to SSSEEE")
-
-        #Test for 2 episodes
-        test_filename_sxxdotexxdotexx = ("test.tv.show.S01."
-                                         "E01.E02.resolution.mp4")
-        test_filename_sxxexxexx = "test.tv.show.S01E01E02.resolution.mp4"
-        warezfile = WarezFile(os.path.join(self.temp_dir,
-                                           test_filename_sxxdotexxdotexx))
-        warezfile.fix_show()
-        self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexxexx,
-                         msg="sSS.eEE.eEE was not converted to sSSeEEeEE")
-
-    def test_fix_show_numbers_only(self):
-        """Test conversion of SEE or SEEEE to sSSeEE or sSSeEEeEE
-        """
-        #Test for 3 digit number
-        test_filename_xxx = "test.tv.show.101.resolution.mp4"
-        test_filename_sxxexx = "test.tv.show.s01e01.resolution.mp4"
-        warezfile = WarezFile(os.path.join(self.temp_dir, test_filename_xxx))
-        warezfile.fix_show()
-        self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexx,
+                         test_filename,
                          msg="SEE was not converted to s0SeEE")
 
-        #Test for 5 digit number
-        test_filename_xxxxx = "test.tv.show.10102.resolution.mp4"
-        test_filename_sxxexxexx = "test.tv.show.s01e01e02.resolution.mp4"
-        warezfile = WarezFile(os.path.join(self.temp_dir, test_filename_xxxxx))
+    def test_fix_show_numbers_only_two_episodes(self):
+        """Test conversion of SEEEE to sSSeEEeEE
+        """
+        test_filename_number = "test.tv.show.10102.resolution.mp4"
+        test_filename = "test.tv.show.s01e01e02.resolution.mp4"
+        warezfile = WarezFile(os.path.join(self.temp_dir, test_filename_number))
         warezfile.fix_show()
         self.assertEqual(os.path.basename(warezfile.filename),
-                         test_filename_sxxexxexx,
+                         test_filename,
                          msg="SEEEE was not converted to s0SeEEeSS")
 
 if __name__ == "__main__":
