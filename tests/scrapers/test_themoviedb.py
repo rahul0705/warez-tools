c618d3b9c41fcb78578c6490d65386d9c70ff300
Rahul Mohandas <rahul0705@gmail.com>
2015-11-14 10:16:20 -0500

    added new unittest for scraper

---
+++ tests/scrapers/test_themoviedb.py
@@ -0,0 +1,23 @@
+"""
+author: Rahul Mohandas
+"""
+import unittest
+import os
+import errno
+import shutil
+import tempfile
+
+from wareztools.scrapers.themoviedb import TheMovieDB
+
+class TestTheMovieDB(unittest.TestCase):
+    """Test the Video class
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
