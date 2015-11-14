29b0b0a523589b679035bafeecb48c0bf48b140e
Rahul Mohandas <rahul0705@gmail.com>
2015-11-14 00:27:45 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 """
 author: Rahul Mohandas
 """
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="warez-tools",
     version="0.1",
-    packages=find_packages(exclude=["tests"]),
+    packages=setuptools.find_packages(exclude=["tests"]),
     author="Rahul Mohandas",
     author_email="rahul@rahulmohandas.com",
     description="Tools to handle Warez Standards",
